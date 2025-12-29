0126c8944116ac16c0d86ec5b79ba025bcdd54e6
didonosocl <donosoveradiego@gmail.com>
2025-12-29 20:16:31 -0300

    endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIndex(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  }
+
+  @Get('styles.css')
+  getStyles(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', 'public', 'styles.css'));
+  }
+
+  @Get('api/categories')
+  async getCategories() {
+    return this.appService.getCategories();
+  }
+
+  @Get('api/stats')
+  async getStats() {
+    return this.appService.getStats();
   }
 }
